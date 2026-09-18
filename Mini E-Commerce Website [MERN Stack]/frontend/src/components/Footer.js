6b102371f4a4e29ba49999f324d2e271615aaeb0
manojofficial019-ui <manojofficial019@gmail.com>
2026-09-18 15:04:01 +0530

    Initial commit

---
+++ Mini E-Commerce Website [MERN Stack]/frontend/src/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return  <footer className="py-1 bg-dark">
+                <p className="text-center text-white mt-1 ">
+                    TOMMY's Cart - 2026-2027, All Rights Reserved
+                </p>
+            </footer>
+}
